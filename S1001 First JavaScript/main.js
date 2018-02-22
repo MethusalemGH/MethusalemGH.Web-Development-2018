11541801c7be670430dab715e18b61a43cd05005
Danilo Lax <DaniloLax@Live.com>
2018-02-22 10:53:20 -0500

    Minor changes to ESLint and First Javascript example

--- S1001 First JavaScript/main.js
+++ S1001 First JavaScript/main.js
@@ -1,2 +1,8 @@
-var myVar = 'Hello World';
-window.alert(myVar);
+// Simple Javascript functions for example
+
+function HelloWorldMessage() { // eslint-disable-line no-unused-vars
+  var myVar = 'Hello World';
+  window.alert(myVar);
+
+  return true;
+};
