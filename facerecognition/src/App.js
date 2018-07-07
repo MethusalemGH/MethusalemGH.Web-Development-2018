2ec3c7f4e641bb33026dc3209809b3d316c23667
Danilo Lax <0180888@itwconnect.com>
2018-07-07 18:39:08 -0400

    Minor changes for resetting image and face boxes

--- facerecognition/src/App.js
+++ facerecognition/src/App.js
@@ -133,11 +133,12 @@ class App extends React.Component {
   }
 
   hideFaceBox = () => {
-    this.setState({ box: { top: 0, left: 0, bottom: 0, right: 0 } });
+    this.setState({ boxes: [] });
   }
 
   loadUser = (user) => {
-    this.setState({ user: user });
+    this.setState({ user: user, input: defaultState.input });
+    this.hideFaceBox();
   }
 }
 
